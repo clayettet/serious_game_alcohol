cecdab6afe4f83b1fa9d909a34187a925aad0011
Theo <theo-clayette@orange.fr>
2018-11-07 14:20:29 +0100

    added better random way to move while drunk (finally)

--- Assets/AlcoholLevel.cs
+++ Assets/AlcoholLevel.cs
@@ -7,20 +7,36 @@ public class AlcoholLevel : MonoBehaviour
 
     [Range(0.0f, 1.0f)]
     public static float level;
+    private float changeMoveIn;
+    private bool switchMove;
 
     // Use this for initialization
     void Start()
     {
         level = 0.4f;
+        changeMoveIn = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Level" + level);
+        Debug.Log(changeMoveIn);
         if (level >= 0.5f) //is Drunk
-        { 
-            transform.position = transform.position + new Vector3(Mathf.Sin(Time.time)*Mathf.PerlinNoise(Time.time,0.0f)/20, 0.0f, Mathf.Sin(Time.time) * Mathf.PerlinNoise(Time.time, 0.0f) / 20); // drunk movement
+        {
+            if (changeMoveIn <= 0.0f)
+            {
+                changeMoveIn = Random.RandomRange(1.0f, 5.0f);
+                switchMove = !switchMove;
+            }
+            if (switchMove)
+            {
+                transform.position = transform.position + new Vector3(Mathf.Sin(Time.time) * Mathf.PerlinNoise(Time.time, 0.0f) / 20, 0.0f, Mathf.Sin(Time.time) * Mathf.PerlinNoise(Time.time, 0.0f) / 20); // drunk movement
+                changeMoveIn = changeMoveIn - Time.deltaTime;
+            } else
+            {
+                transform.position = transform.position + new Vector3(-Mathf.Sin(Time.time) * Mathf.PerlinNoise(Time.time, 0.0f) / 20, 0.0f, Mathf.Sin(Time.time) * Mathf.PerlinNoise(Time.time, 0.0f) / 20); // drunk movement
+                changeMoveIn = changeMoveIn - Time.deltaTime;
+            }
         }
     }
 }

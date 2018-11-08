2461b038cd025002678410c77fd8e7152b279a1c
Theo <theo-clayette@orange.fr>
2018-11-08 15:17:24 +0100

    added a moving target with hitmarker

--- Assets/MovingTarget.cs
+++ Assets/MovingTarget.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class MovingTarget : MonoBehaviour {
 
-	// Use this for initialization
+    public AlcoholLevel AlcoholLevelManager;
+    public TextMesh hit;
+    
+    // Use this for initialization
 	void Start () {
-		
-	}
+        hit.gameObject.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = transform.position + new Vector3(0.0f, 0.0f, Mathf.Sin(Time.time) * Mathf.PerlinNoise(Time.time, 0.0f)/5); // drunk movement
+        if (AlcoholLevel.level < 0.5f)
+        {
+            transform.position = transform.position + new Vector3(0.0f, 0.0f, Mathf.Sin(Time.time) / 10);
+        }
+        else
+        {
+            transform.position = transform.position + new Vector3(0.0f, 0.0f, Mathf.Sin(Time.time) * Mathf.PerlinNoise(Time.time, 0.0f) / 5);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Destroy(col.gameObject);
+        StartCoroutine(DisplayHit());
+
+    }
+
+    IEnumerator DisplayHit()
+    {
+        hit.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1);
+        hit.gameObject.SetActive(false);
     }
 }
